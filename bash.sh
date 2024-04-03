93537bb26ed6976e12b2807a7cd9e1e289c363aa
rrthumma22 <165304235+rrthumma22@users.noreply.github.com>
2024-04-02 23:47:48 -0500

    Update bash.sh

--- bash.sh
+++ bash.sh
@@ -1,4 +1,4 @@
 yum install httpd -y
-echo "<h1> Hi i am linux webserver script running</h1>"> /var/www/html/index.html
+echo "<h1> Hi i am linux webserver script running. this is version</h1>"> /var/www/html/index.html
 service httpd start
 chkconfig httpd on
